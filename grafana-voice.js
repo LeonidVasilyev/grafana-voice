04aeb6a5cc403983666d7cb55eb226c2cc4ba54a
LeonidVasilyev <leonid.vasilyev@outlook.com>
2016-03-15 09:20:33 +0300

    Update grafana-voice.js

--- grafana-voice.js
+++ grafana-voice.js
@@ -1,15 +1,22 @@
 function checkSingleStats() {
+    var isPassed = true;
     var singleStats = document.getElementsByTagName('grafana-panel-singlestat');
     for (var i = 0; i < singleStats.length; ++i) {
         var color = singleStats[i].querySelector('grafana-panel div').style.backgroundColor;
         var title = singleStats[i].querySelector('.panel-title-text').innerText
-            var value = singleStats[i].querySelector('.singlestat-panel-value').innerText
+        var value = singleStats[i].querySelector('.singlestat-panel-value').innerText
+        if (value === "N/A") {
+            value = "not available";
+        }
         if (color === "rgba(245, 54, 54, 0.901961)") {
+            isPassed = false;
             report("DANGER! - " + title + " value is " + value);
         } else if (color === "rgba(237, 129, 40, 0.890196)") {
+            isPassed = false;
             report("WARNING! - " + title + " value is " + value);
         }
     }
+    return isPassed;
 }
 
 function report(message) {
@@ -17,11 +24,15 @@ function report(message) {
     var synth = new SpeechSynthesisUtterance(message);
     synth.voice = window.speechSynthesis.getVoices()[4];
     synth.pitch = 0;
-    synth.rate = 0.8
+    synth.rate = 0.9;
     window.speechSynthesis.speak(synth);
 }
 
 (function(){
-    checkSingleStats();
-    setTimeout(arguments.callee, 10000);
+    if (checkSingleStats()) {
+        setTimeout(arguments.callee, 10000);
+    } else
+    {
+        setTimeout(arguments.callee, 60000);
+    }
 })();
