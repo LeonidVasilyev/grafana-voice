105ba1bb2252845b3759d4939c0618985975c165
LeonidVasilyev <leonid.vasilyev@outlook.com>
2016-03-19 17:21:21 +0300

    New message format

--- grafana-voice.js
+++ grafana-voice.js
@@ -1,82 +1,101 @@
 (function() {
-    var speechApi = window.speechSynthesis;
-    if (speechApi == null) {
-        alert("Web Speech API is not supported by your browser. Please use at least Chrome ver. 33");
-        return;
+  if (window.speechSynthesis == null) {
+    alert("Web Speech API is not supported by your browser. Please use at least Chrome ver. 33");
+    return;
+  }
+  
+  var Status = Object.freeze({OK: 1, WARNING: 2, CRITICAL:3});
+  var ReportType = Object.freeze({NOT_LOADED: 1, NULL_VALUE: 2, VALUE: 3});
+    
+  function checkSingleStats() {
+    if (window.speechSynthesis.paused || window.speechSynthesis.pending || window.speechSynthesis.speaking) {
+      // Workaround strange window.speechSyntesis freeze.
+      // TODO: Fix long phrases cut.
+      window.speechSynthesis.cancel();
     }
     
-    // Settings
-    var warningColor = "rgba(237, 129, 40, 0.890196)";
-    var dangerColor = "rgba(245, 54, 54, 0.901961)";
-    var checkPeriodInSeconds = 10;
-    var delayAfterNotifiationInSeconds = 30;
-    
-    window.onbeforeunload = function() {
-        return "You have attempted to leave this page. Voice alerting will be disabled.";
+    var singleStats = document.getElementsByTagName('grafana-panel-singlestat');
+    var reports = [];
+    for (var i = 0, singleStat; singleStat = singleStats[i]; ++i) {
+      var metric = { 
+        title: singleStat.querySelector('.panel-title-text').innerText, 
+        isLoaded: singleStat.querySelector(".alert-error.panel-error") == null,
+        color: singleStat.querySelector('grafana-panel div').style.backgroundColor,
+        value: singleStat.querySelector('.singlestat-panel-value').innerText
+      };
+      
+      if (!metric.isLoaded) {
+        reports.push({status: Status.WARNING, type: ReportType.NOT_LOADED, metric: metric});
+      } else if (metric.value === "N/A") {
+        reports.push({status: Status.WARNING, type: ReportType.NULL_VALUE, metric: metric});
+      } else if (metric.color === "rgba(237, 129, 40, 0.890196)") {
+        reports.push({status: Status.WARNING, type: ReportType.VALUE, metric: metric});
+      } else if (metric.color === "rgba(245, 54, 54, 0.901961)") {
+        reports.push({status: Status.CRITICAL, type: ReportType.VALUE, metric: metric});
+      }
     }
     
-    function checkSingleStats() {
-        if (speechApi.paused || speechApi.pending || speechApi.speaking) {
-            speechApi.resume();
-        }
-        var isPassed = true;
-        var singleStats = document.getElementsByTagName('grafana-panel-singlestat');
-        for (var i = 0; i < singleStats.length; ++i) {
-            var title = singleStats[i].querySelector('.panel-title-text').innerText
-            
-            var isDataLoaded = singleStats[i].querySelector(".alert-error.panel-error") == null;
-            if (!isDataLoaded) {
-                isPassed = false;
-                report("DANGER! Can't load " + title + " value", true);
-                continue;
-            }
-            
-            var color = singleStats[i].querySelector('grafana-panel div').style.backgroundColor;
-            var value = singleStats[i].querySelector('.singlestat-panel-value').innerText
-            if (value === "N/A") {
-                value = "not available";
-            }
-            if (color === dangerColor) {
-                isPassed = false;
-                report("DANGER! " + title + " value is " + value);
-            } else if (color === warningColor) {
-                isPassed = false;
-                report("WARNING! " + title + " value is " + value);
-            }
-        }
-        return isPassed;
+    if (reports.length === 0) {
+      return true;
     }
     
-    function report(message, useLocalService) {
-        console.log(message);
-        var utterThis = new SpeechSynthesisUtterance(message);
-        var voices = window.speechSynthesis.getVoices();
-        if (useLocalService) {
-            // In case speech paused because of connection issues.
-            utterThis.voice = voices.filter(function(voice) { return voice.localService == true; })[0];
-        } else {
-            utterThis.voice = voices.filter(function(voice) { return voice.lang == "en-US"; })[0];
-        }
-        utterThis.pitch = 0;
-        utterThis.rate = 0.9;
-        utterThis.onerror = function(event) {
-            alert("An error has occurred with the speech synthesis: " + event.error + " Original message: " + message);
-        }
-        speechApi.speak(utterThis);
+    // Sort from most dangerous to least dangerours errors.
+    reports.sort(function(r1, r2) { return r2.status - r1.status;});
+    var message = getLocalizedStatusPrefix(reports[0].status) + ": ";
+    for (var i = 0, report; report = reports[i]; ++i) {
+      message += getLocalizedReportMessage(report) + ", ";
     }
-    
-    // TODO: Hide only specific link
-    // Hide activation link
-    var links = document.getElementsByTagName('dash-link')[0];
-    if (links != undefined) {
-        links.style.display = "none";
+    console.log(message);
+    sayLocalized(message);
+    return false;
+  };
+  
+  function getLocalizedStatusPrefix(status) {
+    if (status === Status.CRITICAL) {
+      return "Danger";
+    } else {
+      return "Warning";
     }
-    
-    (function(){
+  };
+  
+  function getLocalizedReportMessage(report) {
+    if (report.type === ReportType.VALUE) {
+        return report.metric.title + " value is " + report.metric.value;
+      } else if (report.type === ReportType.NULL_VALUE) {
+        return report.metric.title + " is not available";
+      } else if (report.type === ReportType.NOT_LOADED) {
+        return "can't load " + report.metric.title + " value";
+      }
+  };
+  
+  function sayLocalized(message) {
+    var utterThis = new SpeechSynthesisUtterance(message);
+    utterThis.voice = window.speechSynthesis.getVoices()
+      .filter(function(voice) { return voice.voiceURI === "Google UK English Male";})[0];
+    utterThis.onerror = function(event) {
+      alert("An error has occurred with the speech synthesis: " + event.error + " Original message: " + message);
+    }
+    window.speechSynthesis.speak(utterThis);
+  };
+  
+  // Warmup voices array
+  var unused = window.speechSynthesis.getVoices();
+  
+  // TODO: Hide only specific link
+  var links = document.getElementsByTagName('dash-link')[0];
+  if (links != undefined) {
+    links.style.display = "none";
+  }
+  
+  window.onbeforeunload = function() {
+    return "You have attempted to leave this page. Voice alerting will be disabled.";
+  };
+  
+  (function(){
         if (checkSingleStats()) {
-            setTimeout(arguments.callee, checkPeriodInSeconds * 1000);
+            setTimeout(arguments.callee, 10000);
         } else {
-            setTimeout(arguments.callee, delayAfterNotifiationInSeconds * 1000);
+            setTimeout(arguments.callee, 30000);
         }
     })();
 })();
